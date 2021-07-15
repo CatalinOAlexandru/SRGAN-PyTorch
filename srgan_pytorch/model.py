a47a8b09ba4d4e25fc5fce60d478499e7a9747bd
Catalin Alexandru <alex97catalin@yahoo.com>
2021-07-15 21:30:43 +0100

    New model which accepts different scale factors
    modified the model to accept new scale factors

--- srgan_pytorch/model.py
+++ srgan_pytorch/model.py
@@ -12,9 +12,9 @@
 # limitations under the License.
 # ==============================================================================
 import torch
+import math
 from torch import nn
 from torch.hub import load_state_dict_from_url
-from torch import Tensor
 
 __all__ = ["ResidualBlock", "SubpixelConvolutionLayer", "Discriminator",
            "Generator", "discriminator", "generator"]
@@ -28,10 +28,10 @@ class ResidualBlock(nn.Module):
     r""" Residual convolution block.
 
     Args:
-        channels (int): Number of channels in the input image. (Default: 64)
+        channels (int): Number of channels in the input image.
     """
 
-    def __init__(self, channels: int = 64) -> Tensor:
+    def __init__(self, channels: int):
         super(ResidualBlock, self).__init__()
         self.conv1 = nn.Conv2d(channels, channels, 3, 1, 1, bias=False)
         self.bn1 = nn.BatchNorm2d(channels)
@@ -39,11 +39,10 @@ class ResidualBlock(nn.Module):
         self.conv2 = nn.Conv2d(channels, channels, 3, 1, 1, bias=False)
         self.bn2 = nn.BatchNorm2d(channels)
 
-    def forward(self, x: Tensor) -> Tensor:
+    def forward(self, x):
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.prelu(out)
-
         out = self.conv2(out)
         out = self.bn2(out)
 
@@ -56,16 +55,16 @@ class SubpixelConvolutionLayer(nn.Module):
     r""" Sub-pixel upsampled convolution block.
 
     Args:
-        channels (int): Number of channels in the input image. (Default: 64)
+        channels (int): Number of channels in the input image.
     """
 
-    def __init__(self, channels: int = 64) -> None:
+    def __init__(self, channels: int, up_scale):
         super(SubpixelConvolutionLayer, self).__init__()
-        self.conv = nn.Conv2d(channels, channels * 4, 3, 1, 1)
-        self.pixel_shuffle = nn.PixelShuffle(2)
+        self.conv = nn.Conv2d(channels, channels * up_scale ** 2, 3, 1, 1)
+        self.pixel_shuffle = nn.PixelShuffle(up_scale)
         self.prelu = nn.PReLU()
 
-    def forward(self, x: Tensor) -> Tensor:
+    def forward(self, x):
         out = self.conv(x)
         out = self.pixel_shuffle(out)
         out = self.prelu(out)
@@ -75,7 +74,7 @@ class SubpixelConvolutionLayer(nn.Module):
 
 class Discriminator(nn.Module):
 
-    def __init__(self) -> None:
+    def __init__(self):
         super(Discriminator, self).__init__()
         self.features = nn.Sequential(
             # input size. (3) x 96 x 96
@@ -99,10 +98,10 @@ class Discriminator(nn.Module):
             nn.Conv2d(256, 256, 3, 2, 1, bias=False),
             nn.BatchNorm2d(256),
             nn.LeakyReLU(0.2, True),
+            # state size. (512) x 6 x 6
             nn.Conv2d(256, 512, 3, 1, 1, bias=False),
             nn.BatchNorm2d(512),
             nn.LeakyReLU(0.2, True),
-            # state size. (512) x 6 x 6
             nn.Conv2d(512, 512, 3, 2, 1, bias=False),
             nn.BatchNorm2d(512),
             nn.LeakyReLU(0.2, True)
@@ -118,7 +117,7 @@ class Discriminator(nn.Module):
         # Init all model weights.
         self._initialize_weights()
 
-    def forward(self, x: Tensor) -> Tensor:
+    def forward(self, x):
         out = self.features(x)
         out = torch.flatten(out, 1)
         out = self.classifier(out)
@@ -138,7 +137,9 @@ class Discriminator(nn.Module):
 
 
 class Generator(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self,scale_factor):
+        upsample_block_num = int(math.log(scale_factor, 2))
+
         super(Generator, self).__init__()
         # First layer.
         self.conv1 = nn.Sequential(
@@ -160,8 +161,8 @@ class Generator(nn.Module):
 
         # 2 Sub-pixel convolution layers.
         subpixel_conv = []
-        for _ in range(2):
-            subpixel_conv += [SubpixelConvolutionLayer(64)]
+        for _ in range(upsample_block_num):
+            subpixel_conv += [SubpixelConvolutionLayer(64,2)]
         self.subpixel_conv = nn.Sequential(*subpixel_conv)
 
         # Final output layer.
@@ -173,7 +174,7 @@ class Generator(nn.Module):
         # Initializing all neural network weights.
         self._initialize_weights()
 
-    def forward(self, x: Tensor) -> Tensor:
+    def forward(self, x):
         conv1 = self.conv1(x)
         trunk = self.trunk(conv1)
         conv2 = self.conv2(trunk)
@@ -195,6 +196,7 @@ class Generator(nn.Module):
                     nn.init.constant_(m.bias, 0)
 
 
+
 def discriminator() -> Discriminator:
     r"""Build discriminator model from the <https://arxiv.org/abs/1609.04802>` paper.
 
@@ -206,19 +208,20 @@ def discriminator() -> Discriminator:
     return model
 
 
-def generator(pretrained: bool = False, progress: bool = True) -> Generator:
-    r"""Build generator model from the <https://arxiv.org/abs/1609.04802>` paper.
+# def generator(pretrained: bool = False, progress: bool = True, up_scale: int) -> Generator:
+#     r"""Build generator model from the <https://arxiv.org/abs/1609.04802>` paper.
 
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
-    model = Generator()
+#     Args:
+#         pretrained (bool): If True, returns a model pre-trained on ImageNet
+#         progress (bool): If True, displays a progress bar of the download to stderr
+#     """
+#     print('In generator upscale is set to:',up_scale)
+#     model = Generator(up_scale)
 
-    if pretrained:
-        state_dict = load_state_dict_from_url(model_urls["srgan"],
-                                              progress=progress,
-                                              map_location=torch.device("cpu"))
-        model.load_state_dict(state_dict)
+#     if pretrained:
+#         state_dict = load_state_dict_from_url(model_urls["srgan"],
+#                                               progress=progress,
+#                                               map_location=torch.device("cpu"))
+#         model.load_state_dict(state_dict)
 
-    return model
+#     return model
