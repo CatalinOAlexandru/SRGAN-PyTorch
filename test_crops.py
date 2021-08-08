905e9333ad2f53e00f2e3ae78d9c201f3da38ca0
Catalin Alexandru <alex97catalin@yahoo.com>
2021-08-08 19:20:13 +0100

    New patch based testing method.
    This method uses patch based inference to achieve super resolution. It will apply the model on a square part of the original image and put the image back together. Currently it has a very basic implementation and does not do any overlapping.

---
+++ test_crops.py
@@ -0,0 +1,326 @@
+# Copyright 2021 Dakewe Biotech Corporation. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+# Dataset paths:
+# /home/calexand/datasets/Histology/test --- 27 image
+# /home/calexand/datasets/histo_split_4/cropTarget -- 120 images
+
+print('Starting Testing...')
+
+import logging
+import os
+import sys
+from argparse import ArgumentParser
+import matplotlib.pyplot as plt 
+import pandas as pd
+import cv2
+import time
+import numpy as np
+
+import torch
+from PIL import Image
+from skimage.color import rgb2ycbcr
+from skimage.io import imread
+from skimage.metrics import peak_signal_noise_ratio
+from skimage.metrics import structural_similarity
+from torchvision.transforms import ToTensor
+from torchvision.utils import save_image
+
+# from srgan_pytorch.model import Generator
+from srgan_pytorch.utils import create_folder
+
+import lpips
+
+import warnings
+warnings.simplefilter("ignore", UserWarning)
+
+# It is a convenient method for simple scripts to configure the log package at one time.
+logger = logging.getLogger(__name__)
+logging.basicConfig(format="[ %(levelname)s ] %(message)s", level=logging.INFO)
+
+parserTest = ArgumentParser()
+parserTest.add_argument("--pretrained", dest="pretrained", action="store_true", help="Use pre-trained model.")
+parserTest.add_argument("--model-path", default="", type=str, help="Path to latest checkpoint for model.")
+parserTest.add_argument("--model-type", default="model3", type=str, help="Which model file to use. Default: model3")
+# parserTest.add_argument("--test-path-lr", default="data/Set14/LRbicx4", type=str, help="Path to test images")
+parserTest.add_argument("--test-path-hr", default="data/Set14/GTmod12", type=str, help="Path to test images")
+parserTest.add_argument("--scale-factor", default=4, type=int, help="Scale Factor for image")
+parserTest.add_argument("--max-images", default=None, type=int, help="Only run testing on N amount of images instead of the entire test folder.")
+parserTest.add_argument("--num-resBlocks", default=16, type=int, help="Number of Residual Blocks blocks used in the model. Default: 16 as in the paper.")
+parserTest.add_argument("--image-size", default=100, type=int, help="Image size of high resolution image. (Default: 96)")
+parserTest.add_argument("--name", default="DEF", type=str, help="Name for test folder")
+parserTest.add_argument("--cuda", dest="cuda", action="store_true", help="Enables cuda.")
+args = parserTest.parse_args()
+
+if args.model_type == 'model3':
+    from srgan_pytorch.model3 import Generator
+elif args.model_type == 'model2':
+    from srgan_pytorch.model2 import Generator
+elif args.model_type == 'model1':
+    from srgan_pytorch.model1 import Generator
+else:
+    print('Model selected is not available. Use "model1", "model2" or "model3".')
+    sys.exit()
+
+# Set whether to use CUDA.
+device = torch.device("cuda:0" if args.cuda else "cpu")
+print('Using device:',device)
+
+
+def sr(model, hr_filename, sr_filename, cropSize):
+
+    with torch.no_grad():
+        img = cv2.imread(hr_filename) 
+        imgTensor = torch.zeros((img.shape[2],img.shape[1],img.shape[0])).to(device)
+        # imgTensor = imgTensor.permute(0,2,1)
+        # print('input image',img.shape)
+
+        patches = 0
+
+        start = time.time()
+        for i in range(0,img.shape[0],cropSize):
+            for j in range(0,img.shape[1],cropSize):
+                patches+=1
+                newImg = img[i:i+cropSize,j:j+cropSize]
+                # print('input image',newImg.shape)
+
+                downDim = (int(newImg.shape[0]/args.scale_factor),int(newImg.shape[1]/args.scale_factor)) 
+                downImg = cv2.resize(newImg,downDim,interpolation = cv2.INTER_CUBIC)
+                downImg = cv2.cvtColor(downImg, cv2.COLOR_BGR2RGB)
+                downImgTensor = ToTensor()(downImg).unsqueeze(0).to(device)
+                # print('down img',downImg.shape)
+
+                upImg = model(downImgTensor)
+                # upImg = upImg[...,4:-4,4:-4]
+                # upImg = upImg.permute(0,1,3,2)
+                # print(upImg.shape)
+                # upImg = torch.nn.functional.pad(upImg,pad=(4,4,4,4), mode='constant', value=0)
+                # print(upImg.shape)
+                
+                imgTensor[:,i:i+cropSize,j:j+cropSize] = upImg[0]
+        end = time.time()
+        logger.info(f"It took {round(end - start,2)} seconds to process {patches} patches into the SR image.")
+        save_image(imgTensor, sr_filename, normalize=True)
+
+
+# normalize image between xmin and xmax
+def scale(X, x_min, x_max):
+    nom = (X-X.min(axis=0))*(x_max-x_min)
+    denom = X.max(axis=0) - X.min(axis=0)
+    denom[denom==0] = 1
+    return x_min + nom/denom
+
+
+def perpSim(sr_filename, hr_filename,loss_fn_alex):
+    # load image and normalize
+    sr_image = lpips.im2tensor(lpips.load_image(sr_filename)) # RGB image from [-1,1]
+    hr_image = lpips.im2tensor(lpips.load_image(hr_filename))
+
+    d1 = loss_fn_alex(sr_image, hr_image)
+
+    return d1.item()
+
+
+def iqa(sr_filename, hr_filename):
+    r""" Image quality evaluation function.
+
+    Args:
+        sr_filename (str): Super resolution image address.
+        hr_filename (srt): High resolution image address.
+
+    Returns:
+        PSNR value(float), SSIM value(float).
+    """
+    sr_image = imread(sr_filename)
+    hr_image = imread(hr_filename)
+
+    srSize = sr_image.shape[0]
+    hrSize = hr_image.shape[0]
+
+    # Delete 4 pixels around the image to facilitate PSNR calculation.
+    if(srSize == hrSize):
+        sr_image = sr_image[4:-4, 4:-4, ...]
+        hr_image = hr_image[4:-4, 4:-4, ...]
+    else:
+        raise Exception("Difference between SR and HR sizes. Should be equal.")
+    # elif(srSize > hrSize):
+    #     diff = srSize - hrSize
+    #     if diff == 8:
+    #         # images wont be pixel perfect as they are no longer symetric however
+    #         # PSNR should do fine to see similarities
+    #         sr_image = sr_image[4:-4, 4:-4, ...]
+    #     else:
+    #         raise Exception("Difference between SR and HR is not 8. Fix the math.")
+    # else:
+    #     diff = hrSize - srSize # swapped these 2
+    #     if diff == 8:
+    #         # same as above
+    #         hr_image = hr_image[4:-4, 4:-4, ...]
+    #     else:
+    #         raise Exception("Difference between SR and HR is not 8. Fix the math.")
+
+    # Calculate the Y channel of the image. Use the Y channel to calculate PSNR
+    # and SSIM instead of using RGB three channels.
+    sr_image = sr_image / 255.0
+    hr_image = hr_image / 255.0
+    sr_image = rgb2ycbcr(sr_image)[:, :, 0:1]
+    hr_image = rgb2ycbcr(hr_image)[:, :, 0:1]
+    # Because rgb2ycbcr() outputs a floating point type and the range is [0, 255],
+    # it needs to be renormalized to [0, 1].
+    sr_image = sr_image / 255.0
+    hr_image = hr_image / 255.0
+
+    psnr = peak_signal_noise_ratio(sr_image, hr_image)
+    ssim = structural_similarity(sr_image,
+                                 hr_image,
+                                 win_size=11,
+                                 gaussian_weights=True,
+                                 multichannel=True,
+                                 data_range=1.0,
+                                 K1=0.01,
+                                 K2=0.03,
+                                 sigma=1.5)
+
+    return psnr, ssim
+
+
+def saveZoomImages(sr_filename,hr_filename,filename,psnr,ssim,d1):
+    # lr_image = imread(lr_filename)
+
+    sr_image = cv2.cvtColor(cv2.imread(sr_filename), cv2.COLOR_BGR2RGB)
+    hr_image = cv2.cvtColor(cv2.imread(hr_filename), cv2.COLOR_BGR2RGB)
+    dims = (int(hr_image.shape[0]/args.scale_factor),int(hr_image.shape[1]/args.scale_factor))
+    lr_image = cv2.resize(hr_image,dims,interpolation = cv2.INTER_CUBIC)
+
+    cropSize = 50 # 50 in each direction, therefore 100x100 zoom image
+    sf = args.scale_factor # scale factor
+
+    fig, axs = plt.subplots(2, 3)
+    name = 'PSNR: ' + str(round(psnr,2)) + \
+           ' | SSIM: ' + str(round(ssim,2)) + \
+           ' | PerceptSim: '+str(round(d1,2))
+    fig.suptitle(name)
+    lrsize = int(lr_image.shape[0]/2)
+    srsize = int(sr_image.shape[0]/2)
+
+    axs[0,0].imshow(lr_image[lrsize-cropSize:lrsize+cropSize, lrsize-cropSize:lrsize+cropSize, :])
+    axs[0,0].set_title('Low Res')
+    axs[0,0].axis('off')
+    axs[0,1].imshow(sr_image[srsize-(cropSize*sf):srsize+(cropSize*sf), srsize-(cropSize*sf):srsize+(cropSize*sf), :])
+    axs[0,1].set_title('Super Res')
+    axs[0,1].axis('off')
+    axs[0,2].imshow(hr_image[srsize-(cropSize*sf):srsize+(cropSize*sf), srsize-(cropSize*sf):srsize+(cropSize*sf), :])
+    axs[0,2].set_title('Ground Truth')
+    axs[0,2].axis('off')
+
+    axs[1,0].imshow(lr_image)
+    axs[1,0].axis('off')
+    axs[1,1].imshow(sr_image)
+    axs[1,1].axis('off')
+    axs[1,2].imshow(hr_image)
+    axs[1,2].axis('off')
+    plt.tight_layout()
+    fig.tight_layout(rect=[0, 0, 1, 0.97])
+
+    namePath = os.path.join("tests", args.name, 'figs',filename)
+    fig.savefig(namePath,dpi=300)
+    plt.close()
+
+
+
+def main():
+    # Load model and weights.
+    if args.model_type == 'model1':
+        model = Generator(args.scale_factor).to(device).eval()
+    else:
+        model = Generator(args.scale_factor, args.num_resBlocks).to(device).eval()
+
+    if args.model_path != "":
+        logger.info(f"Loading weights from `{args.model_path}`.")
+        model.load_state_dict(torch.load(args.model_path))
+
+    loss_fn_alex = lpips.LPIPS(net='alex') # best forward scores
+
+    # Get test image file index.
+    filenames = os.listdir(os.path.join(args.test_path_hr))
+
+    # for statistics
+    resultScores = {'psnr': [], 'ssim': [], 'perpSimi': []}
+
+    for index in range(len(filenames)):
+        if (args.max_images != None) and args.max_images == index:
+            break
+
+        # print("Image {}/{}".format(index+1,len(filenames)))
+        logger.info(f"Image: {index+1} / {len(filenames)}")
+
+        # lr_filename = os.path.join(args.test_path_lr, filenames[index])
+        sr_filename = os.path.join("tests", args.name, 'full_image', filenames[index])
+        hr_filename = os.path.join(args.test_path_hr, filenames[index])
+
+        # Process low-resolution images into super-resolution images.
+        sr(model, hr_filename, sr_filename, args.image_size)
+
+        # Test the image quality difference between the super-resolution image
+        # and the original high-resolution image.
+        psnr, ssim = iqa(sr_filename, hr_filename)
+        perpSimi = perpSim(sr_filename, hr_filename,loss_fn_alex)
+        logger.info(f"PSNR: {psnr:.2f} | SSIM: {ssim:.4f} | PerceptSim: {perpSimi:.4f}")
+
+        resultScores['psnr'].append(psnr)
+        resultScores['ssim'].append(ssim)
+        resultScores['perpSimi'].append(perpSimi)
+
+        if index < 5:
+            saveZoomImages(sr_filename,hr_filename,filenames[index],psnr,ssim,perpSimi)
+
+
+    # Calculate the average index value of the image quality of the test dataset.
+    avg_psnr = sum(resultScores['psnr']) / len(resultScores['psnr'])
+    avg_ssim = sum(resultScores['ssim']) / len(resultScores['ssim'])
+    avg_percepSim = sum(resultScores['perpSimi']) / len(resultScores['perpSimi'])
+
+    logger.info(f"Mean Average PSNR: {avg_psnr:.2f}dB.")
+    logger.info(f"Mean Average SSIM: {avg_ssim:.4f}.")
+    logger.info(f"Mean Average Perceptual Similarity: {avg_percepSim:.4f}.")
+
+    logger.info(f"PSNR Best / Worst: {np.max(resultScores['psnr']):.2f} / {np.min(resultScores['psnr']):.2f}")
+    logger.info(f"SSIM Best / Worst: {np.max(resultScores['ssim']):.2f} / {np.min(resultScores['ssim']):.2f}")
+    logger.info(f"PercepSim Best/Worst: {np.min(resultScores['perpSimi']):.3f} / {np.max(resultScores['perpSimi']):.3f}")
+
+
+    out_path = 'stats/testing'
+    data_frame_score = pd.DataFrame(
+        data={'PSNR Score': resultScores['psnr'], 'SSIM Score': resultScores['ssim'], 'Perceptual Similarity Score': resultScores['perpSimi']},
+        index=filenames[:len(resultScores['psnr'])])
+    data_frame_score.to_csv(out_path + str(args.name) + '_Testing_Scores.csv', index_label='File Name')
+
+
+if __name__ == "__main__":
+    create_folder("tests")
+    create_folder(os.path.join("tests", args.name))
+    create_folder(os.path.join("tests", args.name, "figs"))
+    create_folder(os.path.join("tests", args.name, "full_image"))
+    create_folder("stats")
+    create_folder("stats/testing")
+
+    logger.info("TrainEngine:")
+    logger.info("\tAPI version .......... 0.4.1")
+    logger.info("\tBuild ................ 2021.07.09")
+    logger.info("\tModified by ... Catalin Alexandru")
+    logger.info("\tOn ................... 2021.07.28")
+
+    main()
+
+    logger.info("All testing has been completed successfully.\n")
