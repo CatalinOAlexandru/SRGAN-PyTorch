689ff211f276774fe6bb25dfc3fd9f3b61827425
Catalin Alexandru <alex97catalin@yahoo.com>
2021-09-17 10:45:46 +0100

    Update dataset.py

--- srgan_pytorch/dataset.py
+++ srgan_pytorch/dataset.py
@@ -29,7 +29,7 @@ from .utils.common import check_image_file
 from .utils.data_augmentation import random_horizontally_flip
 from .utils.data_augmentation import random_vertically_flip
 
-__all__ = ["BaseDataset", "CustomDataset"]
+__all__ = ["BaseDataset"]
 
 
 class BaseDataset(Dataset):
@@ -43,9 +43,12 @@ class BaseDataset(Dataset):
 
     def __init__(self, dataroot: str,
                  image_size: int,
-                 scale: int):
+                 scale: int,
+                 downFrom: int):
         super(BaseDataset, self).__init__()
         lr_image_size = image_size // scale
+        self.imgSize = image_size
+        self.downSize = downFrom
         # Get the index of all images in the directory that meet the suffix format conditions.
         self.filenames = [os.path.join(dataroot, x) for x in
                           os.listdir(dataroot) if check_image_file(x)]
@@ -61,11 +64,21 @@ class BaseDataset(Dataset):
             transforms.RandomVerticalFlip(),
             transforms.ToTensor()
         ])
+        self.hr_downsampled_transform = transforms.Compose([
+            transforms.RandomCrop((downFrom, downFrom)),
+            transforms.Resize((image_size, image_size), IMode.BICUBIC),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomVerticalFlip(),
+            transforms.ToTensor()
+        ])
 
     def __getitem__(self, index):
         hr = Image.open(self.filenames[index]).convert("RGB")
 
-        hr = self.hr_transforms(hr)
+        if self.downSize > 0 and self.downSize > self.imgSize:
+            hr = self.hr_downsampled_transform(hr)
+        else:
+            hr = self.hr_transforms(hr)
         lr = self.lr_transforms(hr)
 
         # Used to save the small patches to see how well the model can test on them
@@ -84,52 +97,52 @@ class BaseDataset(Dataset):
         return len(self.filenames)
 
 
-class CustomDataset(Dataset):
-    r""" Load through the pre-dataset.
+# class CustomDataset(Dataset):
+#     r""" Load through the pre-dataset.
 
-    Args:
-        dataroot (str): The directory address where the data image is stored.
-        image_size (optional, int): The size of image block is randomly cut out from the original image.
-        scale (optional, int): Image magnification.
-        use_da (optional, bool): Do you want to use data enhancement for training dataset. (Default: `True`)
-    """
+#     Args:
+#         dataroot (str): The directory address where the data image is stored.
+#         image_size (optional, int): The size of image block is randomly cut out from the original image.
+#         scale (optional, int): Image magnification.
+#         use_da (optional, bool): Do you want to use data enhancement for training dataset. (Default: `True`)
+#     """
 
-    def __init__(self, dataroot: str,
-                 image_size: int,
-                 scale: int,
-                 use_da: bool = True):
-        super(CustomDataset, self).__init__()
-        lr_image_size = image_size // scale
-        self.use_da = use_da
+#     def __init__(self, dataroot: str,
+#                  image_size: int,
+#                  scale: int,
+#                  use_da: bool = True):
+#         super(CustomDataset, self).__init__()
+#         lr_image_size = image_size // scale
+#         self.use_da = use_da
 
-        # Get the index of all images in the directory that meet the suffix format conditions.
-        self.filenames = os.path.join(dataroot, "input")
-        self.lr_filenames = [os.path.join(dataroot, "input", x) for x in
-                             self.filenames if check_image_file(x)]
-        self.hr_filenames = [os.path.join(dataroot, "target", x) for x in
-                             self.filenames if check_image_file(x)]
+#         # Get the index of all images in the directory that meet the suffix format conditions.
+#         self.filenames = os.path.join(dataroot, "input")
+#         self.lr_filenames = [os.path.join(dataroot, "input", x) for x in
+#                              self.filenames if check_image_file(x)]
+#         self.hr_filenames = [os.path.join(dataroot, "target", x) for x in
+#                              self.filenames if check_image_file(x)]
 
-        self.lr_transforms = transforms.Compose([
-            transforms.CenterCrop((lr_image_size, lr_image_size)),
-            transforms.ToTensor()
-        ])
-        self.hr_transforms = transforms.Compose([
-            transforms.CenterCrop((image_size, image_size)),
-            transforms.ToTensor()
-        ])
+#         self.lr_transforms = transforms.Compose([
+#             transforms.CenterCrop((lr_image_size, lr_image_size)),
+#             transforms.ToTensor()
+#         ])
+#         self.hr_transforms = transforms.Compose([
+#             transforms.CenterCrop((image_size, image_size)),
+#             transforms.ToTensor()
+#         ])
 
-    def __getitem__(self, index):
-        lr = Image.open(self.lr_filenames[index]).convert("RGB")
-        hr = Image.open(self.hr_filenames[index]).convert("RGB")
+#     def __getitem__(self, index):
+#         lr = Image.open(self.lr_filenames[index]).convert("RGB")
+#         hr = Image.open(self.hr_filenames[index]).convert("RGB")
 
-        if self.use_da:
-            lr, hr = random_horizontally_flip(lr, hr)
-            lr, hr = random_vertically_flip(lr, hr)
+#         if self.use_da:
+#             lr, hr = random_horizontally_flip(lr, hr)
+#             lr, hr = random_vertically_flip(lr, hr)
 
-        # Norm HR image [0, 1] to [-1, 1]
-        hr = (hr / 0.5) - 1.
+#         # Norm HR image [0, 1] to [-1, 1]
+#         hr = (hr / 0.5) - 1.
 
-        return lr, hr
+#         return lr, hr
 
-    def __len__(self):
-        return len(self.filenames)
+#     def __len__(self):
+#         return len(self.filenames)
